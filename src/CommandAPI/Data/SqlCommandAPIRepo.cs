78d553588276cac49f03401d47ffa9bb9376e36c
asdap <office@asdap.com>
2021-03-14 15:59:06 +0200

    Added Secrets

--- src/CommandAPI/Data/SqlCommandAPIRepo.cs
+++ src/CommandAPI/Data/SqlCommandAPIRepo.cs
@@ -1,18 +1,26 @@
 using System.Collections.Generic;
+using System.Linq;
 using CommandAPI.Models;
 
 namespace CommandAPI.Data
 {
     public class SqlCommandAPIRepo : ICommandAPIRepo
     {
+        private readonly CommandContext _context;
+
+        public SqlCommandAPIRepo(CommandContext context)
+        {
+            _context = context;
+        }
+
         public IEnumerable<Command> GetAllCommands()
         {
-            throw new System.NotImplementedException();
+            return _context.CommandItems.ToList();
         }
 
         public Command GetCommandById(int id)
         {
-            throw new System.NotImplementedException();
+            return _context.CommandItems.FirstOrDefault(p => p.Id == id);
         }
     }
 }
